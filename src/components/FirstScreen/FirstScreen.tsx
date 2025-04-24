7effd186311131db0587390bb9bd902ca6085431
litvin-av <litvin-anastasiya91@mail.ru>
2025-04-24 17:05:59 +0300

    Created component FirstScreen and added in the Main.

---
+++ src/components/FirstScreen/FirstScreen.tsx
@@ -0,0 +1,11 @@
+import './style.scss';
+
+const FirstScreen = () => {
+    return (
+        <div className="first-screen">
+
+        </div>
+    );
+}
+ 
+export default FirstScreen;
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,8 +1,15 @@
+import FirstScreen from '../FirstScreen/FirstScreen';
 import './style.scss';
 
 const Main = () => {
     return (
-        <main>MAIN</main>
+        <main className="main">
+            <div className="main__wrapper">
+                <div className="main__first-screen">
+                    <FirstScreen />
+                </div>
+            </div>
+        </main>
     );
 }
  
