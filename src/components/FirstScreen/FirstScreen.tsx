9c5a8e6496c73bb8eca24e193bb7f220ee5e0454
litvin-av <litvin-anastasiya91@mail.ru>
2025-04-30 10:30:15 +0300

    Styles for component FirstScreen.

--- src/components/FirstScreen/FirstScreen.tsx
+++ src/components/FirstScreen/FirstScreen.tsx
@@ -1,9 +1,16 @@
+import Title from '../Title/Title';
 import './style.scss';
+
+let textTitle = 'Укрепление здоровья'
 
 const FirstScreen = () => {
     return (
         <div className="first-screen">
-
+            <div className="first-screen__title">
+                <Title textTitle={textTitle} />
+            </div>
+            <div className="first-screen__subtitle">Акватерапия
+            RitmStyle</div>
         </div>
     );
 }
