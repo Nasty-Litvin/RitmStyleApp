be231e5d9d63625eb03fe33d1102a06405855c14
litvin-av <litvin-anastasiya91@mail.ru>
2025-07-31 17:39:57 +0300

    Added changes inthe Footer.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,14 @@ import './style.scss';
 
 const Footer = () => {
     return (
-        <footer className="footer">FOOTER</footer>
+        <footer className="footer">
+            <div className="footer__wrapper">
+                <div className="footer__text">© 2018-2022 RitmStyle</div>
+                <div className="footer__link">
+                    <a href="#">Anastasiya</a>
+                </div>
+            </div>
+        </footer>
     );
 }
  
